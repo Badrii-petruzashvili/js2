9c458671e1a9bbc2dd0856fe0170248890d4cab4
Badrii-petruzashvili <126329141+Badrii-petruzashvili@users.noreply.github.com>
2023-05-03 05:22:20 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict"
+
+// 2.
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// for (let item of array1){
+//     if (item < 10 && item > 0){
+//         console.log(item);
+//     }
+// }
+
+
+// 3.
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let item of array2){
+//     if(array2[item] == 5)
+//     console.log('Aris');
+// }
+
+
+// 4.
+// let array3 = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < array3.length; i++){
+//     sum += array3[i];
+//     }
+// console.log(sum)
+
+
+
+// 6.
+// let array5 = [1, 2, 3, 7, 6, 9];
+// for (let item of array5) {
+//     if (item == 7) {
+//         continue;
+//     }
+//     console.log(item); }
+
+
+
+// 7.
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+//   };
+//   console.log(user.studentstatus);
+
+
+// 11.
+// let users = [
+//     { username: 'giorgi', status: false },
+//     { username: 'levani', status: false },
+//     { username: 'anna', status: true }]
+
+// for (let item in users) {
+//     if (users[item] == 'true')
+//         console.log(users[item]);
+// }
